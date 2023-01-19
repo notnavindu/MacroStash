1bf9fa518d1d39d2d4089acf22b17ca4fa2056c6
Navindu <navanavi3@gmail.com>
2023-01-20 01:53:38 +0530

    feat: view pending users

--- src/routes/(protected)/users/+page.svelte
+++ src/routes/(protected)/users/+page.svelte
@@ -7,12 +7,23 @@
 	import { onMount } from 'svelte';
 
 	let users: User[] = [];
+	let pendingUsers: User[] = [];
+
 	const db = getFirestore();
 
 	let addUserOpen = false;
 
 	onMount(async () => {
 		users = await getDocs(collection(db, 'users')).then((snap) => {
+			return snap.docs.map((user) => {
+				return {
+					id: user.id,
+					...user.data()
+				} as User;
+			});
+		});
+
+		pendingUsers = await getDocs(collection(db, 'pending_users')).then((snap) => {
 			return snap.docs.map((user) => {
 				return {
 					id: user.id,
@@ -27,18 +38,29 @@
 	<AddUserModal />
 {/if}
 
-<div class="w-full flex justify-between items-center">
-	<div class="text-3xl">Users</div>
-	<button
-		on:click={() => (addUserOpen = true)}
-		class="bg-color-blue px-5 py-1 rounded-md btn-glow  transition-all duration-500"
-	>
-		Add
-	</button>
+<div class="text-3xl">Users</div>
+
+<div class="flex flex-wrap gap-4 mt-5 w-full justify-star">
+	{#each users as user (user.id)}
+		<div
+			class="w-fit bg-color-gray-light bg-opacity-20 
+					flex  items-center justify-center gap-4 rounded-lg p-5"
+		>
+			<div class="flex-shrink-0">
+				{@html toSvg(user.name, 70)}
+			</div>
+
+			<div class="flex flex-col items-start pr-3">
+				<div class="text-xl">{user.name}</div>
+				<div class="opacity-50">{user.email}</div>
+			</div>
+		</div>
+	{/each}
 </div>
 
-<div class="flex flex-wrap gap-4 mt-9 w-full justify-star">
-	{#each users as user (user.id)}
+<div class="text-3xl mt-9">Pending Approval</div>
+<div class="flex flex-wrap gap-4 mt-5 w-full justify-star">
+	{#each pendingUsers as user (user.id)}
 		<div
 			class="w-fit bg-color-gray-light bg-opacity-20 
 					flex  items-center justify-center gap-4 rounded-lg p-5"
